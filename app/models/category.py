9a86f46bdda300645ab6ef7784e1c328e0d24c73
SelArt <seleev10@gmail.com>
2025-02-01 22:09:56 +0300

    2commit

--- app/models/category.py
+++ app/models/category.py
@@ -1,11 +1,11 @@
-from app.backend.db import Base
+from backend.db import Base
 from sqlalchemy import Column, ForeignKey, String, Boolean, Integer
 from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
 import os
-
 import sys
 
-sys.path.append(os.getcwd())
+# sys.path.append(os.getcwd())
 class Category(Base):
     __tablename__ = 'categories'
     
@@ -14,5 +14,4 @@ class Category(Base):
     slug = Column(String, unique = True, index =True)
     is_active = Column(Boolean, default=True)
     
-from sqlalchemy.schema import CreateTable
 print(CreateTable(Category.__table__))
